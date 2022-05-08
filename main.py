38a61dda91ca46dd10724defe87245605192e536
Joanna <86994@student.ue.poznan.pl>
2022-05-08 22:19:43 +0200

    Pierwsze podejście

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ class ListTooShortException(Exception):
 def policz(lista, dzialanie):
   if len(lista) < 3:
     raise ListTooShortException("„Błąd: można podać maksymalnie 3 liczby")
-  for len(lista) = 1:
+  for i in lista = 1:
     wynik = 3,14*x*x
   return wynik
-   for len(lista) = 2:
+   for i in lista = 2:
     wynik = (lista, lambda x, y: x * y)
   return wynik
-   for len(lista) = 3:
+   for i in lista = 3:
     wynik = (lista,sqrt((a+b+c)/2*((a+b+c)/2-a)((a+b+c)/2-b)((a+b+c)/2-c) )
   return wynik
