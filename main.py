85c9974d046b068a016081dd869e0555eb62d82c
Joanna <86994@student.ue.poznan.pl>
2022-05-28 09:49:26 +0200

    Drugie podejście

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import math
 
-def policz(*args):
-  if len(args) == 1:
-    wynik = 3,14*pow(args[0], 2)
-    return wynik
-  elif len(args) == 2:
-    wynik = args[0]*args[1]
-    return wynik
-  elif len(args) == 3:
-    p = (args[0] + args[1] + args[2])/2
-    to_sqrt = p * (p-args[0]) * (p-args[1]) * (p-args[2])
-    wynik = math.sqrt(to_sqrt)
-    return wynik
-  else:
-    return "„Błąd: można podać maksymalnie 3 liczby"
+def licz_pole(*args):
+    if len(args) == 1:
+        wynik = math.pi*pow(args[0], 2)
+        return wynik
+    elif len(args) == 2:
+        wynik = args[0]*args[1]
+        return wynik
+    elif len(args) == 3:
+        p = (args[0] + args[1] + args[2])/2
+        to_sqrt = p * (p-args[0]) * (p-args[1]) * (p-args[2])
+        wynik = math.sqrt(to_sqrt)
+        return wynik
+    else:
+        return „Błąd: można podać maksymalnie 3 liczby"
 
 def main():
     liczba_figur = int(input())
@@ -25,13 +25,13 @@ def main():
     for figura in figury:
         figura = figura.split(' ')
         figura = [float(x) for x in figura]
-        pole = policz(*figura)
+        pole = licz_pole(*figura)
         if pole == "Błąd":
             print("Błąd: można podać maksymalnie 3 liczby")
             return
         else:
-            wynik = pole
- 
+            wynik += pole
+        
     wynik = round(wynik, 2)
     print(wynik)
 
